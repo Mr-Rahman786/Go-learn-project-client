1202187cc49042aab16361f1b303376ab1fa2312
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-12-05 12:17:36 +0800

    complete all the submission

--- src/Components/CourseDetails/CourseDetails.js
+++ src/Components/CourseDetails/CourseDetails.js
@@ -30,26 +30,27 @@ const CourseDetails = () => {
 
     return (
         <div ref={ref}>
+            
             <Card className='mx-auto remove-text mainCourseDetails'>
+                
                 <img src={img} alt="" />
+                <ReactToPdf x={-25} y={.5} scale={0.8} targetRef={ref} filename="div-blue.pdf">
+                    {({ toPdf }) => (
+                        <button className='bg-dark text-light w-50 py-3 rounded' onClick={toPdf}> <FaDownload className='text-warning' /> Download The Bouchar</button>
+                    )}
+                </ReactToPdf>
                 <Card.Body>
+                    
                     <Card.Title>{title}</Card.Title>
                     <Card.Text>
                         {details}
                     </Card.Text>
                     <div className='d-flex justify-content-between link-behavior'>
-                        <div>
-                            <Link to="/courses">Go to courses</Link>
+                        <div className='bg-primary p-2 rounded'>
+                            <Link className='text-light' to="/courses">Go to courses</Link>
                         </div>
-                        <div>
+                        <div className='bg-dark p-2 rounded text-light'>
                             <Link to={`/premium/${id}`}>Access Premium</Link>
-                        </div>
-                        <div >
-                            <ReactToPdf  x={-25} y={.5} scale={0.8} targetRef={ref} filename="div-blue.pdf">
-                                {({ toPdf }) => (
-                                    <button onClick={toPdf}> <FaDownload /> </button>
-                                )}
-                            </ReactToPdf>
                         </div>
                     </div>
                 </Card.Body>
